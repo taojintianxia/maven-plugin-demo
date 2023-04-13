48e2242e20a7a58e298350527e33d76d2cec1e23
shardingsphere <dev@shardingsphere.apache.org>
2023-04-13 17:35:15 +0800

    Add : add more plugin into demo

---
+++ src/main/java/com/github/taojintianxia/mavenplugin/demo/test/sub/SubE2EITDemo.java
@@ -0,0 +1,11 @@
+package com.github.taojintianxia.mavenplugin.demo.test.sub;
+
+/**
+ * Sub E2E Test Demo
+ */
+public class SubE2EITDemo {
+    
+    public static void main(String... args) {
+        System.out.println("Hello World! Sub Test");
+    }
+}
