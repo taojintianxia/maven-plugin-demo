b544ca3dd715c0fc5795ada0b4bc30611bef5d2b
shardingsphere <dev@shardingsphere.apache.org>
2023-04-14 09:11:14 +0800

    Add : add sub module to test spotbug

---
+++ infra/infra-common/src/main/java/com/github/mavenplugin/demo/infra/common/CommonDemo.java
@@ -0,0 +1,11 @@
+package com.github.mavenplugin.demo.infra.common;
+
+/**
+ * Common Demo
+ */
+public class CommonDemo {
+    
+    public void print() {
+        System.out.println("Hello World! Common");
+    }
+}
---
+++ infra/infra-common/src/test/java/com/github/mavenplugin/demo/infra/common/CommonDemoTest.java
@@ -0,0 +1,11 @@
+package com.github.mavenplugin.demo.infra.common;
+
+/**
+ * Common Demo Test
+ */
+public class CommonDemoTest {
+    
+    public void testPrint() {
+        System.out.println("Hello World! Common");
+    }
+}
